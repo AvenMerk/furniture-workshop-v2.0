83c5b09c97441e15ab86e97bc1a6069681243fb5
AvenMerk <kremnevaaj@gmail.com>
2018-08-27 17:17:32 +0300

    Add get for employees page

---
+++ src/actions/employeesAction.js
@@ -0,0 +1,22 @@
+export const REQUEST_EMPLOYEES = 'REQUEST_EMPLOYEES';
+export const RECEIVE_EMPLOYEES = 'RECEIVE_EMPLOYEES';
+
+export const requestEmployees = () => ({
+    type: REQUEST_EMPLOYEES
+});
+
+export const receiveEmployees = (json) => ({
+    type: RECEIVE_EMPLOYEES,
+    employees: json,
+    receivedAt: Date.now()
+});
+
+export const fetchEmployees = () => (dispatch) => {
+    dispatch(requestEmployees());
+    return fetch(`http://furniture-service.herokuapp.com/employee`)
+        .then(
+            response => response.json(),
+            error => console.log("Something went wrong", error)
+        )
+        .then(json => dispatch(receiveEmployees(json)))
+};
---
+++ src/components/EmployeesPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmployeesGet from "../containers/EmployeesGet";
+
+const EmployeesPage = () => (
+    <div>
+        <h3>Our masters</h3>
+        <EmployeesGet />
+    </div>
+
+);
+
+export default EmployeesPage;
