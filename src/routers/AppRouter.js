4accfb724020287fde80a6a33a416285e49721b9
AvenMerk <kremnevaaj@gmail.com>
2018-08-27 16:57:00 +0300

    Add employees page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from '../components/HomePage';
-// import Employees from '../components/Employees';
+import Employees from '../components/Employees';
 // import AboutPage from '../components/AboutPage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
@@ -13,7 +13,7 @@ const AppRouter = () => (
             <Header />
             <Switch>
                 <Route path="/" component={HomePage} exact={true} />
-                {/*<Route path="/employees" component={Employees} />*/}
+                <Route path="/employees" component={Employees} />
                 {/*<Route path="/about" component={AboutPage} />*/}
                 {/*<Route path="/categories" component={Categories} />*/}
                 <Route component={NotFoundPage} />
