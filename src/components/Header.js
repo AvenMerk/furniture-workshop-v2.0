c6df84ac177ea179313d6ceb700bf5bb1939c2d2
AvenMerk <kremnevaaj@gmail.com>
2018-08-28 16:10:46 +0300

    Add About us page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => (
         {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
         <Link to="/" className="is-active option__text" exact={'true'}>Home</Link>
         <Link to="/employees" className="is-active option__text">Our craftsmen</Link>
-        {/*<Link to="/about" className="is-active option__text">About</Link>*/}
+        <Link to="/about" className="is-active option__text">About</Link>
     </header>
 );
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from '../components/HomePage';
 import EmployeesPage from '../components/EmployeesPage';
-// import AboutPage from '../components/AboutPage';
+import AboutPage from '../components/AboutUsPage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -14,7 +14,7 @@ const AppRouter = () => (
             <Switch>
                 <Route path="/" component={HomePage} exact={true} />
                 <Route path="/employees" component={EmployeesPage} />
-                {/*<Route path="/about" component={AboutPage} />*/}
+                <Route path="/about" component={AboutPage} />
                 {/*<Route path="/categories" component={Categories} />*/}
                 <Route component={NotFoundPage} />
             </Switch>
