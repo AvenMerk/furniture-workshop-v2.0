55716632d635bad7a87707253a9b13f9d8429650
AvenMerk <kremnevaaj@gmail.com>
2018-08-29 17:03:55 +0300

    Made a filter for categories (to show products for certain category)

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Category = ({category}) => (
     <ul>
         {category.map((categ, index) =>
             <li key={index}>
-                <button>{`id: ${categ.id}, name: ${categ.name}`}</button>
+                <button id={categ.id}
+                    onClick={() => { window.location.replace('/products');
+                        document.cookie = "id = " + categ.id + ";path=/"}
+                    }>{`id: ${categ.id}, name: ${categ.name}`}</button>
             </li>
         )}
     </ul>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
 
+const cookie = Number(document.cookie.replace(/(?:(?:^|.*;\s*)id\s*\=\s*([^;]*).*$)|^.*$/, "$1"));
 const Products = ({products}) => (
     <ul>
-        {products.map((product, index) =>
+        {products.filter(product => product.category_id === cookie).map((product, index) =>
             <li key={index}>
                 {`id: ${product.id}, name: ${product.name}`}
             </li>
