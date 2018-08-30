67cd331ea47883c2ceb528781cc769bc824f162f
AvenMerk <kremnevaaj@gmail.com>
2018-08-30 20:56:03 +0300

    rename about page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import AboutPage from '../containers/AboutUsPage';
+import AboutPage from '../containers/AboutPage';
 import NotFoundPage from '../containers/NotFoundPage';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
