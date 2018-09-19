3a88268d9033d79456c298572e90eec528b2b9de
AvenMerk <kremnevaaj@gmail.com>
2018-09-19 15:44:35 +0300

    Add styles to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
     <footer className="footer">
         <div className="footer__text">
-            <p>My profile, git etc</p>
+            <p>My profile, git etc.</p>
         </div>
     </footer>
 );
