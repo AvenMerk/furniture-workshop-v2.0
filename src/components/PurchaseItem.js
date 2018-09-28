5dd150bbe4b9de168ed8e72f2297f47554cadebb
AvenMerk <kremnevaaj@gmail.com>
2018-09-28 16:35:20 +0300

    Add restrictions for input number

--- src/components/PurchaseItem.js
+++ src/components/PurchaseItem.js
@@ -4,7 +4,7 @@ import React from 'react';
 const PurchaseItem = ({productId, quantity, onChange}) => (
     <li className="list__style space">
         <p>product: {productId}</p>
-        <input className="add-option__cart" type="number" value={quantity} onChange={onChange}/>
+        <input className="add-option__cart" type="number" min="0" max="100" step="1" value={quantity} onChange={onChange}/>
     </li>
 );
 
