8d4e14ab1537fa59942400e5fa821b0ecffc01c8
AvenMerk <kremnevaaj@gmail.com>
2018-09-28 16:43:15 +0300

    Rename one of links

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
         {/*<div className="logo"></div>*/}
         <header className="header">
             <p id="logo">LOGO</p>
-            <Link to="/" className="is-active header__text" exact={'true'}>Home</Link>
+            <Link to="/" className="is-active header__text" exact={'true'}>Categories</Link>
             <Link to="/about" className="is-active header__text">About</Link>
             <Link to="/cart" className="is-active header__text">Cart</Link>
         </header>
