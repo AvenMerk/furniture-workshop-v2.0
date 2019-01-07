52eaafd051db3bdeb12be65deec0cf9103b118ff
AvenMerk <kremnevaaj@gmail.com>
2019-01-07 17:26:56 +0300

    Add about info

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const AboutPage = () => (
-    <div className="main workshop-page-container"><h2 className="workshop-title">About us</h2></div>
+    <div className="main workshop-page-container"><h2 className="workshop-title">About us</h2>
+        <p>This project was made by
+            <a href="https://github.com/AvenMerk" className="workshop-footer-link"> AvenMerk</a> (front-end)
+            and
+            <a href="https://github.com/Fedor-Bystrov" className="workshop-footer-link"> Fedor Bystrov </a>
+            (back-end).
+        </p>
+        <p>We kindly ask you not to provide any type of personal information.</p>
+        <p>This project is not intended to collect any kind of information or earn money.
+            The only goal of this project is testing skills.</p>
+    </div>
 );
 
 export default AboutPage;
