27e77c16084ef7a4fd497c34549a1471a9c21f98
AvenMerk <kremnevaaj@gmail.com>
2019-01-07 17:29:44 +0300

    Add git links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => (
     <footer className="workshop-footer">
         <div className="workshop-footer-text">
-            <p>Git: <a href="https://github.com/AvenMerk" className="workshop-footer-link">AvenMerk</a></p>
+            <p>GitHub:
+                <a href="https://github.com/AvenMerk" className="workshop-footer-link"> AvenMerk </a>
+                &
+                <a href="https://github.com/Fedor-Bystrov" className="workshop-footer-link"> Fedor Bystrov</a>
+            </p>
         </div>
     </footer>
 );
