3834e84d2901ed86ceda5aae0d2085f1c49b3bcc
AvenMerk <kremnevaaj@gmail.com>
2019-01-07 18:10:57 +0300

    Add git info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,7 @@ const Footer = () => (
     <footer className="workshop-footer">
         <div className="workshop-footer-text">
             <p>GitHub:
-                <a href="https://github.com/AvenMerk" className="workshop-footer-link"> AvenMerk </a>
-                &
-                <a href="https://github.com/Fedor-Bystrov" className="workshop-footer-link"> Fedor Bystrov</a>
+                <a href="https://github.com/AvenMerk" className="workshop-footer-link"> AvenMerk</a>
             </p>
         </div>
     </footer>
--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -3,10 +3,7 @@ import React from 'react';
 const AboutPage = () => (
     <div className="main workshop-page-container"><h2 className="workshop-title">About us</h2>
         <p>This project was made by
-            <a href="https://github.com/AvenMerk" className="workshop-footer-link"> AvenMerk</a> (front-end)
-            and
-            <a href="https://github.com/Fedor-Bystrov" className="workshop-footer-link"> Fedor Bystrov </a>
-            (back-end).
+            <a href="https://github.com/AvenMerk" className="workshop-footer-link"> AvenMerk</a> (front-end).
         </p>
         <p>We kindly ask you not to provide any type of personal information.</p>
         <p>This project is not intended to collect any kind of information or earn money.
