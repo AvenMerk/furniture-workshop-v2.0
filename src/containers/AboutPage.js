e3c58a7ee8cf88f60f3c71ea925eac8080479ace
AvenMerk <36073382+AvenMerk@users.noreply.github.com>
2019-01-08 14:40:47 +0300

    Update manifest.json and improve about page

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -3,11 +3,10 @@ import React from 'react';
 const AboutPage = () => (
     <div className="main workshop-page-container"><h2 className="workshop-title">About us</h2>
         <p>This project was made by
-            <a href="https://github.com/AvenMerk" className="workshop-footer-link"> AvenMerk</a> (front-end).
+            <a href="https://github.com/AvenMerk" className="workshop-footer-link"> AvenMerk</a> (Alexandra Bystrova).
         </p>
         <p>We kindly ask you not to provide any type of personal information.</p>
-        <p>This project is not intended to collect any kind of information or earn money.
-            The only goal of this project is testing skills.</p>
+        <p>This project is not intended to collect any kind of information or earn money.</p>
     </div>
 );
 
